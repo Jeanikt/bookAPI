3b7b56e8ca201b77777c911f585241c02b9aa35a
anaba <anaba@ANA>
2023-04-21 20:37:14 -0300

    Adicionar arquivos de projeto.

---
+++ bookAPI/Model/Book.cs
@@ -0,0 +1,11 @@
+﻿namespace bookAPI.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Autor { get; set; }
+        public string Descrição { get; set; }
+        public string Title { get; set; }
+
+    }
+}
